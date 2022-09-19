3530259bc51199dc508e55f7a0e80210f5862e20
jsantore <jsantore@bridgew.edu>
2022-09-19 19:47:20 -0400

    add remove demo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 students = ["Nathan", "Kelvin", "Mariama", "Barry", "Tori",
             "Michael", "Dominic", "Jose", "Colin", "Emily", "John"]
-
+students.append("Pascal")
+students.insert(0, "Dan")
 print(students[-4])
